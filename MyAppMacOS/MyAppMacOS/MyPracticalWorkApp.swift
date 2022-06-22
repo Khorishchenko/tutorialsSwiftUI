e6bb228e31816b208a50d4da4d8d5c288e8a43f9
Sergey <34285925+Khorishchenko@users.noreply.github.com>
2022-06-22 15:52:41 +0300

    all
    SwiftUI

---
+++ MyAppMacOS/MyAppMacOS/MyPracticalWorkApp.swift
@@ -0,0 +1,20 @@
+//
+//  MyPracticalWorkApp.swift
+//  MyPracticalWork
+//
+//  Created by user on 19.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct MyPracticalWorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            //            ContentView()
+            //            ContectView2()
+            //            ContentView3()
+            ContentView4()
+        }
+    }
+}
